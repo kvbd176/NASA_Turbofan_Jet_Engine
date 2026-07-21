fc885ca4ded3c25c75e09a5cf4501ea894531f2f
Kothamasu.Nikitha <kothamasunikitha@gmail.com>
2026-07-21 14:37:04 +0530

    Update report_generation_agent.py

--- report_generation_agent.py
+++ report_generation_agent.py
@@ -4,16 +4,49 @@ class ReportGenerationAgent:
 
     def generate(self, df):
 
+        # Latest record of each engine
+        latest_df = (
+            df.sort_values("cycle")
+              .groupby("engine_id")
+              .tail(1)
+        )
+
         report = {
-            "Total_Engines": df["engine_id"].nunique(),
-            "Healthy": (df["Health_Status"] == "Healthy").sum(),
-            "Degrading": (df["Health_Status"] == "Degrading").sum(),
-            "Critical": (df["Health_Status"] == "Critical").sum(),
-            "Faults_Detected": (df["Fault_Status"] == "Fault Detected").sum(),
-            "High_Risk": (df["Risk_Level"] == "High").sum(),
-            "Immediate_Maintenance":
-                (df["Maintenance_Priority"] == "Immediate").sum(),
-            "Average_RUL": round(df["Predicted_RUL"].mean(), 2)
+
+            # Engine statistics
+            "Total_Engines": latest_df["engine_id"].nunique(),
+
+            "Healthy_Engines":
+                (latest_df["Health_Status"] == "Healthy").sum(),
+
+            "Degrading_Engines":
+                (latest_df["Health_Status"] == "Degrading").sum(),
+
+            "Critical_Engines":
+                (latest_df["Health_Status"] == "Critical").sum(),
+
+            "Faulty_Engines":
+                (latest_df["Fault_Status"] == "Fault Detected").sum(),
+
+            "High_Risk_Engines":
+                (latest_df["Risk_Level"] == "High").sum(),
+
+            "Immediate_Maintenance_Engines":
+                (latest_df["Maintenance_Priority"] == "Immediate").sum(),
+
+            # Row statistics
+            "Healthy_Rows":
+                (df["Health_Status"] == "Healthy").sum(),
+
+            "Degrading_Rows":
+                (df["Health_Status"] == "Degrading").sum(),
+
+            "Critical_Rows":
+                (df["Health_Status"] == "Critical").sum(),
+
+            "Average_RUL":
+                round(latest_df["Predicted_RUL"].mean(), 2)
+
         }
 
         report_df = pd.DataFrame(
