cca5f5df0ace265ec01cc6c8ec9311fea3f2711f
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2026-08-04 13:09:24 +0530

    Update HealthChart.jsx

--- frontend/src/components/HealthChart.jsx
+++ frontend/src/components/HealthChart.jsx
@@ -14,50 +14,42 @@ const COLORS = [
   "#c4b5fd",
   "#e5e7eb",
 ];
+const [loading,setLoading] = useState(true);
+const [error,setError] = useState(false);
 
 function HealthChart() {
-
   const [data,setData]=useState([]);
-
   useEffect(()=>{
-
     async function load(){
-
-      const res=await API.get(
-        "/analytics/health-distribution"
-      );
-
-      const chartData=Object.entries(
-        res.data
-      ).map(([name,value])=>({
-        name,
-        value
-      }));
-
-      setData(chartData);
-
-    }
-
-    load();
-
+ try{
+  const res = await API.get(
+    "/analytics/health-distribution"
+  );
+  const chartData = Object.entries(res.data)
+  .map(([name,value])=>({
+      name,
+      value
+  }));
+  setData(chartData);
+ }catch(err){
+  console.error(err);
+  setError(true);
+ }
+ finally{
+  setLoading(false);
+ }
+}
+  load();
   },[]);
-
+  
   return(
-
     <div className="bg-white rounded-xl border p-5">
-
       <h2 className="font-semibold mb-4">
-
         Health Status
-
       </h2>
-
       <div className="h-56">
-
         <ResponsiveContainer>
-
           <PieChart>
-
             <Pie
               data={data}
               dataKey="value"
@@ -65,32 +57,21 @@ function HealthChart() {
               innerRadius={55}
               outerRadius={80}
             >
-
               {
                 data.map((entry,index)=>
-
                   <Cell
                     key={index}
                     fill={COLORS[index]}
                   />
-
                 )
               }
-
             </Pie>
-
             <Tooltip/>
-
           </PieChart>
-
         </ResponsiveContainer>
-
       </div>
-
     </div>
-
   );
-
 }
 
 export default HealthChart;
