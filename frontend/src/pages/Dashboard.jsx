a299a01352f22aec42c58bdce96a635fc147509e
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2026-08-04 13:24:04 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import DashboardCards from "../components/DashboardCards";
@@ -7,26 +5,15 @@ import HealthChart from "../components/HealthChart";
 import RiskChart from "../components/RiskChart";
 import EngineTable from "../components/EngineTable";
 import ChatBox from "../components/ChatBox";
-import FullPageLoader from "../components/FullPageLoader";
-
 
 function Dashboard() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 800);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <FullPageLoader />;
-  }
-
   return (
     <div className="flex h-screen bg-gray-100 overflow-hidden">
+      {/* Sidebar */}
       <Sidebar />
+      {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Navbar */}
         <Navbar />
         <div className="flex flex-1 overflow-hidden">
           <main className="flex-1 overflow-y-auto p-6 bg-gray-100">
@@ -45,4 +32,5 @@ function Dashboard() {
     </div>
   );
 }
+
 export default Dashboard;
