217054bf84a2e71bbdf660682b2db547906003a8
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2026-08-04 13:07:16 +0530

    Update DashboardCards.jsx

--- frontend/src/components/DashboardCards.jsx
+++ frontend/src/components/DashboardCards.jsx
@@ -9,7 +9,7 @@ import {
 import API from "../services/api";
 
 function DashboardCards() {
-
+  const [loading, setLoading] = useState(true);
   const [kpis, setKpis] = useState({
     healthy: "--",
     critical: "--",
@@ -18,28 +18,22 @@ function DashboardCards() {
   });
 
   useEffect(() => {
-
     const fetchKPIs = async () => {
       try {
-
         const response = await API.get("/dashboard/kpis");
-
-        console.log("KPIs:", response.data);
-
         setKpis({
           healthy: response.data.healthy_engines ?? "--",
           critical: response.data.critical_engines ?? "--",
           average_rul: response.data.average_rul ?? "--",
           maintenance: response.data.immediate_maintenance_engines ?? "--",
         });
-
       } catch (error) {
         console.error("Failed to fetch KPIs:", error);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchKPIs();
-
   }, []);
 
   const cards = [
@@ -67,29 +61,34 @@ function DashboardCards() {
 
   return (
     <div className="grid grid-cols-2 gap-5">
-      {cards.map((card, index) => (
+      {cards.map((card,index)=>(
         <div
           key={index}
-          className="bg-white rounded-xl border border-gray-200 p-4 shadow-sm hover:shadow-md transition"
+          className="bg-white rounded-xl border border-gray-200 p-4 shadow-sm"
         >
-          <div className="flex justify-between items-center">
-            <div>
-              <p className="text-gray-500 text-sm">
-                {card.title}
-              </p>
-
-              <h2 className="text-2xl font-bold text-gray-800 mt-3">
-                {card.value}
-              </h2>
+          {loading ? (
+            <div className="animate-pulse">
+              <div className="h-4 bg-gray-200 rounded w-24"></div>
+              <div className="h-8 bg-gray-200 rounded w-16 mt-4"></div>
             </div>
-
-            <div>{card.icon}</div>
-
-          </div>
+          ) : (
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-gray-500 text-sm">
+                  {card.title}
+                </p>
+                <h2 className="text-2xl font-bold text-gray-800 mt-3">
+                  {card.value}
+                </h2>
+              </div>
+              <div>
+                {card.icon}
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
   );
 }
-
 export default DashboardCards;
